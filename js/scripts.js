8fb0b71fe0efd307ba5ad2ed134668bc071f5553
unknown <rafuzelz2007@gmail.com>
2025-02-26 16:02:02 -0300

    Versão Final

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('.container');
+const qrCodeBtn = document.querySelector('#qr-form button');
+const qrCodeInput = document.querySelector('#qr-form input');
+const qrCodeImg = document.querySelector('#qr-code img');
+//eventos
+
+//Gerar QR
+function generateQrCode() {
+    const qrCodeInputValue = qrCodeInput.value;
+    if (!qrCodeInputValue) return;
+
+    qrCodeBtn.innerHTML = "Gerando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add('active');
+        qrCodeBtn.innerHTML = "Código gerado!";
+    })
+}
+
+
+
+qrCodeBtn.addEventListener("click", () => {
+generateQrCode()
+})
+
+//Limpar QR Input
+qrCodeInput.addEventListener('keyup', () => {
+    if(!qrCodeInput.value) {
+        container.classList.remove('active');
+        qrCodeBtn.innerText = "Gerar QR Code";
+    }
+})
